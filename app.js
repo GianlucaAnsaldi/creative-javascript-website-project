da9b94d46a8ecac8fa1c92b9ff498a8815179896
Gianluca <gianluca.ansaldi.1989@gmail.com>
2022-11-02 23:32:04 +0000

    toggle burger

--- app.js
+++ app.js
@@ -58,8 +58,10 @@ function animateSlides() {
   });
 }
 
-let mouse = document.querySelector('.cursor');
-let mouseTxt = mouse.querySelector('span');
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
+const navBar = document.querySelector('.nav-bar');
 
 function cursor(e) {
   mouse.style.top = e.pageY + 'px';
@@ -84,6 +86,35 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains('active')) {
+    e.target.classList.add('active');
+    gsap.to('.line1', 0.5, { rotate: 45, y: 5, background: 'black' });
+    gsap.to('.line2', 0.5, {
+      rotate: -45,
+      y: -5,
+      background: 'black',
+    });
+    gsap.to('#logo', 1, { color: 'black' });
+    // navBar.classList.add('show');
+    gsap.to('.nav-bar', 1, { clipPath: 'circle( 2500px at 100% -10%' });
+    document.body.classList.add('hide');
+  } else {
+    e.target.classList.remove('active');
+    gsap.to('.line1', 0.5, { rotate: 0, y: 0, background: 'white' });
+    gsap.to('.line2', 0.5, {
+      rotate: 0,
+      y: 0,
+      background: 'white',
+    });
+    gsap.to('#logo', 1, { color: 'white' });
+    // navBar.classList.add('show');
+    gsap.to('.nav-bar', 1, { clipPath: 'circle( 50px at 100% -10%' });
+    document.body.classList.remove('hide');
+  }
+}
+
+burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
